4eb68943dd3ddc38ec8a97220de63aaf35687ce8
DumpA1n <dengjiang0331@gmail.com>
2024-09-22 20:56:07 +0800

    改进Log模块

--- utils/log.h
+++ utils/log.h
@@ -11,22 +11,15 @@
 #include <string>
 
 std::string getTimeStamp() {
-    // 获取当前时间点
-    auto now = std::chrono::system_clock::now();
-    
-    // 转换为 time_t 类型以格式化日期和时间
-    auto timeT = std::chrono::system_clock::to_time_t(now);
-    
-    // 获取当前时间的毫秒部分
-    auto duration = now.time_since_epoch();
-    auto millis = std::chrono::duration_cast<std::chrono::milliseconds>(duration).count() % 1000;
-
-    // 格式化日期和时间
-    std::tm tm = *std::localtime(&timeT);
+    std::time_t t = std::time(nullptr);
+    std::tm* now = std::localtime(&t);
     std::ostringstream oss;
-    oss << std::put_time(&tm, "%m-%d %H:%M:%S"); // 格式化 MM-DD HH:MM:SS
-    oss << '.' << std::setfill('0') << std::setw(3) << millis; // 添加毫秒部分
-
+    oss << (now->tm_year + 1900) << '-'
+        << (now->tm_mon + 1) << '-'
+        << now->tm_mday << ' '
+        << now->tm_hour << ':'
+        << now->tm_min << ':'
+        << now->tm_sec;
     return oss.str();
 }
 
@@ -41,6 +34,11 @@ public:
         infoS += getTimeStamp() + " [I] " + info;
         infoS += "\n";
     }
+    void LOGE(std::string info) {
+        infoV.push_back(info);
+        infoS += getTimeStamp() + " [E] " + info;
+        infoS += "\n";
+    }
     void clear() {
         infoV.clear();
         infoS.clear();
@@ -48,6 +46,7 @@ public:
 } mlog;
 
 #define LOGI(info) mlog.LOGI(info)
+#define LOGE(info) mlog.LOGE(info)
 
 
 
